ce3f9db40f18d85d0a8f089cfaabb9e72b752eef
phoenix2219 <72683957@continental.edu.pe>
2025-12-04 19:25:24 -0500

    Actualizacion de linea a la url del backend

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,5 +1,5 @@
 export async function predecir(datos) {
-  const res = await fetch("http://localhost:8000/predict", {
+  const res = await fetch("https://clinicaamanecer.onrender.com/predict", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(datos),
